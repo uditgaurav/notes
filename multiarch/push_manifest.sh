5e453504bd650a4c1244c86738b290e608516ae9
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2021-07-06 20:06:59 +0530

    Update push_manifest.sh

--- multiarch/push_manifest.sh
+++ multiarch/push_manifest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This is an experimental feature of docker.To enable experimental features in the Docker CLI, 
 # edit the config.json (https://docs.docker.com/engine/reference/commandline/cli/#configuration-files)
-# and set experimental to enabled. You can go here for more information.
+# and set experimental to enabled. export DOCKER_CLI_EXPERIMENTAL=enabled
+# Add "experimental": "enabled", to ~/.docker/config.json (default location) at the beginning of the file and not at the end.
 
 setup(){
 
@@ -27,9 +28,11 @@ prepare_indivisual_manifest(){
 
    for val in ${platforms[@]}; do
      arch=$(echo ${val} | cut -d / -f2)
+     echo "pulling ${LITMUS_REPONAME}/${IMGNAME}:${IMGTAG} --platform ${val}"
      docker pull ${LITMUS_REPONAME}/${IMGNAME}:${IMGTAG} --platform ${val}
      IMAGEID=$( docker images -q ${LITMUS_REPONAME}/${IMGNAME}:${IMGTAG} )
      docker tag ${IMAGEID} ${TARGET_REPONAME}/${TARGET_IMGNAME}:${arch}
+     echo "pushing ${TARGET_REPONAME}/${TARGET_IMGNAME}:${arch}"
      docker push ${TARGET_REPONAME}/${TARGET_IMGNAME}:${arch}
    done
 }
@@ -45,12 +48,13 @@ create_and_retag_multiarch_manifest(){
         cmd="${cmd} -a ${TARGET_REPONAME}/${TARGET_IMGNAME}:${arch}"
     done
 
-    echo $cmd""
+    echo "Running: docker manifest create ${TARGET_REPONAME}/${TARGET_IMGNAME}:${TARGET_IMGTAG} ${cmd}"
     docker manifest create \
     ${TARGET_REPONAME}/${TARGET_IMGNAME}:${TARGET_IMGTAG} ${cmd}  
 }
 
 push_multiarch_manifest(){
+    echo "pushing multiarch image"
     docker manifest push ${TARGET_REPONAME}/${TARGET_IMGNAME}:${TARGET_IMGTAG}
 }
 
